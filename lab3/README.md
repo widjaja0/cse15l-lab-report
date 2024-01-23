77ef05ec60c49d9644db8c70d84b6614f5204863
widjaja <62527551+widjaja0@users.noreply.github.com>
2024-01-22 21:36:55 -0800

    update

---
+++ lab3/README.md
@@ -0,0 +1,9 @@
+To run the server:
+```bash
+javac Server.java Program.java
+java NumberServer [port]
+//e.g: java NumberServer 8080
+```
+
+“`wavelet`” – noun; a small wave of water; a ripple (Oxford Dictionary). A cute name for a little web server.  
+Original template from UCSD (Joe)
---
+++ lab3/SearchEngine.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> listOfSearches = new ArrayList<>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+                else if (parameters[0].equals("s")) {
+                    listOfSearches.add(parameters[1]);
+                    return String.format("Searched for %s", parameters[1]);
+                }
+            }
+            else if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                String searches = "";
+                if (parameters[0].equals("s")) {
+                    for (int i = 0; i < listOfSearches.size(); i++) {
+                        if (listOfSearches.get(i).contains(parameters[1])) {
+                            searches += listOfSearches.get(i) + "\n";
+                        }
+                    }
+                }
+                return searches;
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
